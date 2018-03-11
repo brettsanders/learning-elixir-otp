ada3352014962b79e121043253cd42bad753732f
Brett Sanders <brettrsanders@gmail.com>
2018-03-11 00:25:45 -0500

    learn nest modules

--- length_converter.ex
+++ length_converter.ex
@@ -1,5 +1,13 @@
-defmodule MeterToFootConverter do
-  def convert(m) do
-    m * 3.28084
+defmodule MetertoLengthConverter do
+  defmodule Feet do
+    def convert(m) do
+      m * 3.28084
+    end
+  end
+
+  defmodule Inch do
+    def convert(m) do
+      m * 39.3701
+    end
   end
 end
